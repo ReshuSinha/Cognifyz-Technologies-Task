7e2e670fa118dc0dab01ca535a51a9de64f46adb
Reshu <165784473+ReshuSinha@users.noreply.github.com>
2024-06-03 11:53:41 +0530

    Add files via upload

---
+++ Level-1/Palindromechecker.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Palindromechecker{
+    public static void check(String q){
+        String p="";         //taken two empty string
+        String a="";       
+        String w=q.toLowerCase();      //converting whole string into lowercase
+        for(int i=0;i<w.length();i++){
+            int t=w.length()-i-1;
+            if((Character.isLetter(w.charAt(t)))){    //Checking whether at index t of given string is a character or not
+                p=p+w.charAt(t);
+            }
+            if((Character.isLetter(w.charAt(i)))){
+                a=a+w.charAt(i);
+            }
+        }
+        if((a.compareTo(p))==0){       //Comapring to string
+            System.out.println("The word or phrase "+q+" is a PALINDROME.");
+        }
+        else{
+            System.out.println("The word or phrase "+q+" is NOT A PALINDROME.");
+        }
+    }
+    public static void main(String args[]){
+        Scanner sn=new Scanner(System.in);
+        System.out.println("Enter the word or phrase you want to check:");
+        String s=sn.nextLine();
+        check(s);
+    }
+}
